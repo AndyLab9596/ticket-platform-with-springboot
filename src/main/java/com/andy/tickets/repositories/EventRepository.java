46617a3fed2ecc1a2bab91e01df0135509a50e04
MAC <mac@192.168.2.202>
2025-08-08 05:07:10 +0700

    [feat] list published events

--- src/main/java/com/andy/tickets/repositories/EventRepository.java
+++ src/main/java/com/andy/tickets/repositories/EventRepository.java
@@ -1,6 +1,7 @@
 package com.andy.tickets.repositories;
 
 import com.andy.tickets.domain.entities.Event;
+import com.andy.tickets.domain.enums.EventStatusEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,5 @@ import java.util.UUID;
 public interface EventRepository extends JpaRepository<Event, UUID> {
     Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
     Optional<Event> findByIdAndOrganizerId(UUID id, UUID organizerId);
+    Page<Event> findByStatus(EventStatusEnum status, Pageable pageable);
 }
